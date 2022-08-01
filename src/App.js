eaf03b6968aca7858bf8e87be866cfddf581a79e
ujjwalpandey922 <104821808+ujjwalpandey922@users.noreply.github.com>
2022-08-01 14:45:05 +0530

    Any message - what you have done since your last commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return (
-      <div>
-        <Navbar />
-      </div>
-    );
+    return <div>
+      <Navbar>
+    </div>;
   }
 }
